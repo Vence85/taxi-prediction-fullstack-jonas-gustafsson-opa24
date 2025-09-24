16f9cc9ed8b73c8d9c560e8d0ccdfeb837ab40e9
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-09-24 19:40:57 +0200

    Implemented /taxi/ endpoint to serve cleaned dataset

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,10 +1,36 @@
 from fastapi import FastAPI
 from taxipred.backend.data_processing import TaxiData
+from pydantic import BaseModel, Field
+from taxipred.models import MODEL_PATH
+import joblib
+import pandas as pd
+from taxipred.backend.data_processing import TaxiData
 
+model = joblib.load(MODEL_PATH)
 app = FastAPI()
-
 taxi_data = TaxiData()
 
+class TaxiInput(BaseModel):
+    Trip_Distance_km: float = Field (gt=0)
+    Time_of_Day: str
+    Day_of_Week: str
+    Passenger_Count: int = Field (gt=0, lt=5)
+
+class PredictionOutput(BaseModel):
+    Predicted_Trip_Price: float
+
 @app.get("/taxi/")
-async def read_taxi_data():
+def read_taxi_data():
     return taxi_data.to_json()
+   
+@app.post("/predict")
+def predict_price(payload: TaxiInput):
+    data_to_predict = pd.DataFrame(payload.model_dump(), index=0)
+    data_to_predict = pd.get_dummies(data_to_predict, drop_first=True)
+    model = joblib.load(MODEL_PATH)
+    data_to_predict = data_to_predict.reindex(columns=model.feature_name_in, fill_value=0)
+    prediciton = model.predict(data_to_predict)[0]
+
+    return {"Predicted trip price": round(prediciton, 0)}                                         
+    
+    
--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,7 @@
 from importlib.resources import files
-
+from pathlib import Path
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
+MODEL_PATH = Path(__file__).resolve().parent.parent / "models" / "taxi_price_predictor.joblib"
 
 # DATA_PATH = Path(__file__).parents[1] / "data"
+print(MODEL_PATH)
