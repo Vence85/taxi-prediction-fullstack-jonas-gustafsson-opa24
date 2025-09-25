8eb84a993a3568f286071e8457e62753bd4cf58c
Jonas Gustafsson <jonas.gustafsson85@gmail.com>
2025-09-25 14:49:37 +0200

    Time and date in streamlite!

--- src/taxipred/frontend/dashboard.py
+++ src/taxipred/frontend/dashboard.py
@@ -2,19 +2,27 @@ import streamlit as st
 from taxipred.utils.helpers import read_api_endpoint
 import pandas as pd
 import requests
+import streamlit as st
+import datetime
 
 data = read_api_endpoint("taxi")
 df = pd.DataFrame(data.json())
 
 def main():
-    st.markdown("# Taxi Prediction Dashboard")
-    # Testa /taxi/
-    st.dataframe(df)
+    
 
-    # Testa /taxi/predict
-    st.markdown("---")
-    st.subheader("Snabb prediktionstest")
-  
+    # Välj datum
+    date = st.date_input(
+        "Välj datum för resan",
+        datetime.date.today()
+    )
+
+    # Välj tid
+    time = st.time_input(
+        "Välj tid för resan",
+        datetime.datetime.now().time()
+    )
+
 
 
 if __name__ == "__main__":
